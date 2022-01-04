142c548b9566ed22474a6ce4b25c59327727a237
Laurent <laurent.sem@edu.itescia.fr>
2022-01-04 12:23:17 +0100

    Script Question 2 done

--- question2/launch.sh
+++ question2/launch.sh
@@ -5,7 +5,7 @@ path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 docker run -d --rm \
     --name nginx_question2 \
     -p 8080:80 \
-    -v $path/index2.html:/user/share/nginx/html/index2.html \
+    -v $path/index.html:/usr/share/nginx/html/index.html \
     nginx:stable
 
 echo "http://localhost:8080/"

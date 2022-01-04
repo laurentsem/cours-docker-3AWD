9bdca2f0f63b705e63c88c0f50675bd9b6b0112e
Clément Sabourin <clement.sabourin@hetic.net>
2022-01-04 13:09:15 +0100

    add script and index.php

---
+++ question3/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+docker network create bridge net_q3
+
+docker run -d \
+    --name nginx_question3 \
+    -p 8080:80 \
+    --network = net_q3
+    nginx:stable
+
+docker run -d \
+    --name php_question3 \
+    --network = net_q3
+    php:8-fpm
+
+echo "http://localhost:8080/"

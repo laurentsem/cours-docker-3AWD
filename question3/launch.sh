1e41de632cf06c70f40bc29db32f0dc0ad9d6b1f
clement.sabourin <clement.sabourin@eemi.com>
2022-01-09 18:29:36 +0100

    fix error

--- question3/launch.sh
+++ question3/launch.sh
@@ -5,13 +5,13 @@ path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 docker network create -d bridge network_question3
 
 docker run -d --rm \
-    --name php_exo3 \
+    --name php_question3 \
     --network=network_question3 \
     -v $path/index.php:/var/www/html/index.php \
     php:8-fpm
 
 docker run -d --rm \
-    --name nginx_exo3 \
+    --name nginx_question3 \
     --network=network_question3 \
     -p 8080:80 \
     -v $path/nginx.conf:/etc/nginx/conf.d/default.conf \
